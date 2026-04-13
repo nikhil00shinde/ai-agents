e8238534f697c3f8b3cdede7eed63cbfb34b4ca7
nikhil00shinde <nikhilshinde00@outlook.com>
2026-04-13 18:29:19 -0400

    added: get file information

---
+++ functions/get_file_content.py
@@ -0,0 +1,8 @@
+from functions.get_files_info import get_files_info
+import os
+
+def get_file_content(working_directory,file_path):
+    try:
+        result = get_files_info(working_directory,file_path)
+        if result 
+            return 
